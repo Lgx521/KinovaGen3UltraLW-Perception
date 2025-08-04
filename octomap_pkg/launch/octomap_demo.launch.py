2cf0152a5a9ddaa02fa29475aec2eee20676a4b3
sgan roar <shengzhegan04@gmail.com>
2025-08-04 15:19:58 -0400

    relocate

---
+++ octomap_pkg/launch/octomap_demo.launch.py
@@ -0,0 +1,37 @@
+# my_robot_launch/launch/octomap.launch.py
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    config_file = os.path.join(
+        get_package_share_directory('octomap_pkg'),
+        'config',
+        'octomap_config.yaml'
+    )
+
+    octomap_server_node = Node(
+        package='octomap_server',
+        executable='octomap_server_node',
+        name='octomap_server',
+        output='screen',
+        parameters=[config_file],
+        remappings=[
+            ('cloud_in', '/camera/depth/color/points')
+        ]
+    )
+    
+
+#    rviz_node = Node(
+#        package='rviz2',
+#        executable='rviz2',
+#        name='rviz2',
+#        arguments=['-d', os.path.join(get_package_share_directory('my_robot_config'), 'rviz', #'octomap.rviz')] 
+    )
+
+    return LaunchDescription([
+        octomap_server_node,
+#        rviz_node
+    ])
